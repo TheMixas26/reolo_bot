892a5dec5922ff4a939fc3e9ada2bc173787bf5b
Амодерни Боровски <AmoBoro@reolo-empire.ru>
2026-09-02 14:28:55 +0300

    guys idk what the duck is this

--- weather.py
+++
@@ -1,9 +0,0 @@
-import requests
-from data import predlojka_bot, chat_mishas_den, admin
-
-def get_weather():
-  return weather
-  
-def send_weather():
-  text = f"Дорогие подписчики! Прогноз погоды на сегодня: {get_weather()}"
-  predlojka_bot.send_message(chat_mishas_den, text)
